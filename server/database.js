addaaee10fb453079d43da4f58a1bf786b947c25
Michael Chiang <michiang@ucsd.edu>
2017-02-15 11:17:44 -0800

    Add MongoDB connection and schema skeleton

---
+++ server/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const databasepath = 'mongodb://localhost/community';
+mongoose.connect(databasepath);
+const db = mongoose.connection;
+
+let userSchema = mongoose.Schema({
+
+})
+
+var User = mongoose.model("User", userSchema)
+
+module.exports = User;
