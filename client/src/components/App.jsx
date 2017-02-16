47e67ff208946af682b210f9b6660f03e907baf0
Michael Chiang <michiang@ucsd.edu>
2017-02-15 20:15:48 -0800

    App stateful component skeleton

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,2 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+}
+
+render() {
+  return (
+
+  );
+}
+
+window.App = App;
 
