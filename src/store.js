13eef2689e9ff43682f31105466dcc254281946d
Michael Chiang <michiang@ucsd.edu>
2017-02-18 15:41:57 -0800

    Experiment with dispatch, getState and Provider on store

--- src/store.js
+++ src/store.js
@@ -1,21 +1,45 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import createLogger from 'redux-logger';
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import createLogger from 'redux-logger'
+import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
+import { render } from 'react-dom'
+import React from 'react'
+import app from './reducers/index.js'
+import { addReview } from './actions/index.js'
 
 //redux-logger creates a logger in the console
 //applyMiddleware function from redux that applies npm modules
 
-//combineReducers
-export default combineReducers({
-  app,
-  routing: routerReducer
-})
+//{subscribe, dispatch, getState}
 
-
+/*
+createStore(reducer, [preloadedState], [enhancer])
+Creates a redux store that holds the complete state tree of app
+reducer - function that returns next state tree given the current state
+and an action to handle
+preloadedState - the intial state
+enhancer - store enhancer/action
+Returns an object that holds complete state of app
+*/
 const store = () => {
   const logger = createLogger();
-  return createStore(rootReducer, applyMiddleware(logger));
+  return createStore(app, applyMiddleware(logger));
 }
+
+//console.log(store.getState());
+
+store.dispatch(addReview(text));
+
+console.log(store.getState())
+
+/*
+Provider component makes props available to every component in app
+*/
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
 export default store;
