98a7815118cc6eac9b54e11e9d773bd4e98fcf5c
Brian Hassett <bchasset@gmail.com>
2017-02-18 17:46:26 -0800

    Added route to get neighborhoods

---
+++ server/apihelpers.js
@@ -0,0 +1,21 @@
+//xml parser -- converts xml to JS object
+var parseString = require('xml2js').parseString;
+var request = require('request');
+
+var zillowApiKey = 'X1-ZWz1fnqmwi7h8r_2qbsp'//process.env.ZILLOW_API_KEY;
+
+exports.getZillowHoods = function(city, state, callback) {
+  var options = {
+    url: 'http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=' + zillowApiKey + '&state=' + state + '&city=' + city + '&childtype=neighborhood'
+  }
+  console.log(options);
+  request(options, function(err, res, body) {
+    if(err) {
+      callback(err, null);
+    } else {
+      parseString(body, function(err, obj) {
+        callback(null, obj['RegionChildren:regionchildren'].response[0].list[0].region);
+      });
+    }
+  });
+}
--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
-
+var request = require('request');
+var apiHelpers = require('./apihelpers.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -22,5 +23,17 @@ app.listen(port, function() {
 app.get('/', function(req,res){
   console.log("get happened")
 })
+
+app.get('/api/neighborhoods/searchbycity/:city/:state', function(req, res) {
+  var city = req.params.city;
+  var state = req.params.state;
+  apiHelpers.getZillowHoods(city, state, function(err, hoods) {
+    if(err) {
+      res.sendStatus(404);
+    } else {
+      res.json(hoods);
+    }
+  });
+});
 
 module.exports = app;
