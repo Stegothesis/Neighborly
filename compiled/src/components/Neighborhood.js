f83e260502c6ea6fef9557c51386db2efabfb38d
Bruce <brucegraham11@gmail.com>
2017-02-20 08:48:32 -0800

    Added folder for Babel to compile files into.

---
+++ compiled/src/components/Neighborhood.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Neighborhood = function Neighborhood(props) {
+  return _react2.default.createElement(
+    'h1',
+    null,
+    'Hello, Neighborhood'
+  );
+};
+
+module.exports = Neighborhood;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9jb21wb25lbnRzL05laWdoYm9yaG9vZC5qc3giXSwibmFtZXMiOlsiTmVpZ2hib3Job29kIiwicHJvcHMiLCJtb2R1bGUiLCJleHBvcnRzIl0sIm1hcHBpbmdzIjoiOztBQUFBOzs7Ozs7QUFFQSxJQUFNQSxlQUFlLFNBQWZBLFlBQWUsQ0FBQ0MsS0FBRCxFQUFXO0FBQzlCLFNBQU87QUFBQTtBQUFBO0FBQUE7QUFBQSxHQUFQO0FBQ0QsQ0FGRDs7QUFJQUMsT0FBT0MsT0FBUCxHQUFpQkgsWUFBakIiLCJmaWxlIjoiTmVpZ2hib3Job29kLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JztcblxuY29uc3QgTmVpZ2hib3Job29kID0gKHByb3BzKSA9PiB7XG4gIHJldHVybiA8aDE+SGVsbG8sIE5laWdoYm9yaG9vZDwvaDE+O1xufVxuXG5tb2R1bGUuZXhwb3J0cyA9IE5laWdoYm9yaG9vZDsiXX0=
