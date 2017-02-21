af189a2b6acc03ab01d24d0cb0e4daee05776bcf
Michael Chiang <michiang@ucsd.edu>
2017-02-20 19:46:13 -0800

    Renders App on DOM wrapped in Provider to pass down store props

---
+++ src/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { IndexRoute, Link, Router, Route, browserHistory } from 'react-router';
+import App from './components/App.jsx'
+import Landing from './components/Landing.jsx'
+import City from './components/City.jsx'
+import Neighborhood from './components/Neighborhood.jsx'
+import Rate from './components/Rate.jsx'
+import User from './components/User.jsx'
+import makeStore from './store.jsx'
+import { syncHistoryWithStore } from 'react-redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers/index.jsx'
+
+//const history = syncHistoryWithStore(browserHistory, store);
+
+//console.log(store.getState());
+
+//Get app id from static html page
+const app = document.getElementById('app');
+
+//Provider sends props to all components in the app
+ReactDOM.render(
+  <Provider store={makeStore(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('app'));
+
+/*
+const Routes = () =>
+<Provider store={store}>
+  <App />
+  <Router history={history}>
+    <Route
+      path="/"
+      component={ App } />
+
+    <Route
+      path="/city"
+      component={ City }/>
+
+    <Route
+      path="/neighborhood"
+      component={ Neighborhood }/>
+
+    <Route
+      path="/rate"
+      component={ Rate  }/>
+
+    <Route
+      path="/user"
+      component={ User }/>
+
+    </Router>
+  </Provider>;
+
+
+export default Routes;
+*/
