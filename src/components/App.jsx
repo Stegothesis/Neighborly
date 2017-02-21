64b677a4887c61b7b57e99caf9584773b52018a4
Renata Maraj <renatavmaraj@gmail.com>
2017-02-20 23:32:39 -0800

    Add UI to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import City from './City.jsx'
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
       <Review />
       <Landing />
       <Neighborhood />
--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const City = (props) => {
-  return <h1>Hello City</h1>;
+  return <div>
+    <ul className="list-group">
+  <li className="list-group-item">Downtown Austin</li>
+  <li className="list-group-item">South Congress</li>
+  <li className="list-group-item">East Austin</li>
+  <li className="list-group-item">Hyde Park</li>
+  <li className="list-group-item">Mueller Community</li>
+</ul>
+  </div>
 }
 
 module.exports = City;
