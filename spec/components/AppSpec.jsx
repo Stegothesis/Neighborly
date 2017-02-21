da31c17475e62cf6d4a6372010c475060be6049f
Michael Chiang <michiang@ucsd.edu>
2017-02-20 23:06:55 -0800

    Integrate app spec test to test if component renders

--- spec/components/AppSpec.jsx
+++ spec/components/AppSpec.jsx
@@ -1,7 +1,21 @@
-describe('App', function() {
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/App.jsx';
 
+describe('App', function() {
   it('should be a stateful class component', function() {
     expect(React.Component.isPrototypeOf(App)).to.be.true;
   });
-
 });
+
+describe('App' , () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
+
+  it('renders something', () => {
+    expect(component).to.exist;
+  });
+});
+
