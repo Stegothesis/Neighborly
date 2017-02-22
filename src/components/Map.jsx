42c4f1f681a4d9892ed1b3a4cb29ce61abe3672c
Bruce <brucegraham11@gmail.com>
2017-02-22 00:55:57 -0800

    Updated Map component to include further styling and formatting.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const Maps = (props) => {
-  return <div>
-    <div>
-      <img src={'http://www.ariozick.com/wp-content/uploads/2009/11/austin.png'} className="img-responsive"/>
-    </div>;
+  return <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <div className="carousel-inner" role="listbox">
+      <div className="item active">
+        <img className="first-slide" src="https://cmgstatesmanaustin.files.wordpress.com/2015/09/austin.png" />
+        <div className="container">
+        </div>
+      </div>
+    </div>
+    <a className="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span className="sr-only">Previous</span>
+    </a>
+    <a className="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span className="sr-only">Next</span>
+    </a>
   </div>
 }
 
