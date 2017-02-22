020772c52b1abb00621cea3bd5652b1c1e7d74e2
Bruce <brucegraham11@gmail.com>
2017-02-21 19:27:02 -0800

    Adding ESLint file

--- compiled/src/constants/actionTypes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var ADD_REVIEW = exports.ADD_REVIEW = 'ADD_REVIEW';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9jb25zdGFudHMvYWN0aW9uVHlwZXMuanN4Il0sIm5hbWVzIjpbIkFERF9SRVZJRVciXSwibWFwcGluZ3MiOiI7Ozs7O0FBQU8sSUFBTUEsa0NBQWEsWUFBbkIiLCJmaWxlIjoiYWN0aW9uVHlwZXMuanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgQUREX1JFVklFVyA9ICdBRERfUkVWSUVXJyJdfQ==
