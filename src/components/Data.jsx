b0ca5fb2fc449edff6bde2d046614b0a9c20e40e
Bruce <brucegraham11@gmail.com>
2017-02-22 00:56:30 -0800

    Updated the Data component with furhter styling and formatting.

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
-export const Data = (props) => {
-  return <div>
-    <h1>Data Component Here</h1>
+const Data = (props) => {
+  return <div className='jumbotron'>
+    <div className="row marketing">
+      <div className="col-lg-6">
+        <h4>API Data #1</h4>
+        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+        <h4>API Data #2</h4>
+        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+        <h4>API Data #3</h4>
+        <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+      </div>
+
+      <div className="col-lg-6">
+        <h4>API Data #4</h4>
+        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+        <h4>API Data #5</h4>
+        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+        <h4>API Data #6</h4>
+        <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+      </div>
+    </div>
   </div>
 }
+
+module.exports = Data;
