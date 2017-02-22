acc178c770110ce08bcf5f711e35fd622c29746a
Bruce <brucegraham11@gmail.com>
2017-02-21 19:27:48 -0800

    Adding compiled folder got .gitignore

---
+++ src/components/Data.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Data = (props) => {
+  return <div>
+    <h1>Data Component Here</h1>
+  </div>
+}
---
+++ src/components/Map.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Maps = (props) => {
+  return <div>
+    <div>
+      <img src={'http://www.ariozick.com/wp-content/uploads/2009/11/austin.png'} className="img-responsive"/>
+    </div>;
+  </div>
+}
+
+module.exports = Maps;
