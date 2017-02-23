5ffd88e06a81296502f9279b027061a72641410e
Renata Maraj <renatavmaraj@gmail.com>
2017-02-23 00:14:38 -0800

    Remove static map

---
+++ src/routes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/App.jsx';
+
+export default (
+<Route path="/" component={App} />
+);
+
+// heroku-communityapp.com/ => Renders App
