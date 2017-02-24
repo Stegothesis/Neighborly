b35544f01aa41af11dc3b9c9d8c4b7be51013784
Bruce <brucegraham11@gmail.com>
2017-02-24 10:18:13 -0800

    Resolved all git conflicts and grunt build bugs.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import ReviewSubmit from '../containers/ReviewSubmit.jsx'
-import ReviewMap from '../containers/ReviewMap.jsx'
-import Neighborhood from './Neighborhood.jsx'
-import City from '../containers/City.jsx'
-import Rate from './Rate.jsx'
-import User from './User.jsx'
-import SearchBar from '../containers/SearchBar.jsx'
-import NeighborhoodDetail from '../containers/neighborhood-detail.jsx'
-import NavbarContainer from '../containers/NavbarContainer.jsx'
+import ReviewSubmit from '../containers/ReviewSubmit.jsx';
+import ReviewMap from '../containers/ReviewMap.jsx';
+import Neighborhood from './Neighborhood.jsx';
+import City from '../containers/City.jsx';
+import Rate from './Rate.jsx';
+import User from './User.jsx';
+import SearchBar from '../containers/SearchBar.jsx';
+import NeighborhoodDetail from '../containers/neighborhood-detail.jsx';
+import NavbarContainer from '../containers/NavbarContainer.jsx';
 import Footer from './Footer.jsx';
 import Header from './Header.jsx';
+import Landing from './Landing.jsx';
 
 export default class App extends React.Component {
   render() {
@@ -22,23 +23,10 @@ export default class App extends React.Component {
         </div>
 
         <div>
-          <Neighborhood />
+          <Landing />
         </div>
 
-        <div>
-          <SearchBar />
-        </div>
-
-        <div>
-          <ReviewSubmit/>
-        </div>
-
-        <div>
-          <City />
-        </div>
-
-        <div>
-        </div>
+        <hr />
 
         <div>
           <NeighborhoodDetail />
@@ -47,6 +35,7 @@ export default class App extends React.Component {
         <div>
           <Footer />
         </div>
+
       </div>
     );
   }
