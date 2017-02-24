480a0df371b58bbec16f8a0b6643f78d01f933d2
Brian Hassett <bchasset@gmail.com>
2017-02-24 15:08:16 -0800

    Add auth0 lock--can log in and out

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,19 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import Auth0Lock from 'auth0-lock'
+// import { EventEmitter } from 'events'
+// import { browserHistory } from 'react-router'
 
 export default class Auth extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    //Auth0
+    super()
+    //let HOST_URL = process.env.NODE_ENV | 'http://localhost:8080';
+    this.lock = new Auth0Lock(process.env.CLIENT_ID, process.env.CLIENT_DOMAIN, {
+      auth: {
+        //redirectUrl: HOST_URL,
+        responseType: 'token',
+        params: {
+          scope: 'openid name email'
+        }
+      }
+    });
+    // this.event = new EventEmitter();
+    this.lock.on('authenticated', this.authenticateUser.bind(this));
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
   }
-  showAuthenticate () {
-    //Show the authentication window
-    this.props.lock.show();
-  }
+
+authenticateUser(result) {
+  this.setToken(result.idToken);
+  this.lock.getUserInfo(result.accessToken, (error, profile) => {
+    if (error) {
+      console.log('Error', error);
+    } else {
+      this.setProfile(profile);
+    }
+  });
+  // browserHistory.replace({pathname: '/#/dashboard'});
+}
+
+setProfile(profile) {
+  localStorage.setItem('profile', JSON.stringify(profile));
+  // this.event.emit('profile_update', profile);
+}
+
+setToken(idToken) {
+  //Store token in localStorage
+  localStorage.setItem('token', idToken);
+}
+
+getProfile(callback) {
+  const profile = localStorage.getItem('profile');
+  return profile ? JSON.parse(profile) : {};
+}
+
+getToken() {
+  return localStorage.getItem('token');
+}
+
+login () {
+  this.lock.show();
+}
+
+loggedIn() {
+  return !!this.getToken();
+}
+
+logout () {
+  localStorage.removeItem('token');
+  localStorage.removeItem('profile');
+  browserHistory.replace('/');
+}
 
   render() {
     return (
       <div className="login-auth">
-        <a onClick = {this.showAuthenticate.bind(this)}> Sign In</a>
+        <a onClick = {this.login.bind(this)}>Sign In </a>
+        <a onClick = {this.logout.bind(this)}> Sign Out</a>
         </div>
       );
     }
