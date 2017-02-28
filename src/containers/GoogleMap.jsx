e4c714d3964b6de49cb63e2cf6d5e30be3c2af67
Michael Chiang <michiang@ucsd.edu>
2017-02-27 22:25:44 -0800

    Implement sync setState to update coord, set default coord after search

--- src/containers/GoogleMap.jsx
+++ src/containers/GoogleMap.jsx
@@ -4,9 +4,11 @@ import { connect } from 'react-redux';
 export class GoogleMap extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      setCoordinates: {lat: 30.0, lng: -97.0}
+      setCoordinates: {lat: 30, lng: -97}
     }
+
   }
 
   componentDidMount() {
@@ -21,21 +23,38 @@ export class GoogleMap extends Component {
     console.log('Set Coordinates', this.state.setCoordinates);
   }
 
+  componentWillReceiveProps(nextProps) {
+    var context = this;
+    console.log('nextProps', nextProps);
+    console.log('UPDATE DEFAULT COORDINATES****', nextProps.defaultCoordinate.lat);
+    if (nextProps && parseFloat(nextProps.defaultCoordinate.lat) !== this.state.setCoordinates.lat) {
+      context.setState( {setCoordinates: {
+        lat: parseFloat(nextProps.defaultCoordinate.lat),
+        lng: parseFloat(nextProps.defaultCoordinate.lng)
+      }
+    }, () => {
+      context.componentDidMount();
+     });
+    }
+  }
+
   componentDidUpdate() {
+    console.log('GOOGLE MAP COMPONENT UPDATED');
     this.updateSetCoordinates();
   }
 
   updateSetCoordinates() {
     var context = this;
-    console.log('New Coordinates', parseFloat(this.props.activeNeighborhood.latitude));
-    console.log('Current Coordinates', this.state.setCoordinates.lat);
+    if (this.props.activeNeighborhood) {
     if (parseFloat(this.props.activeNeighborhood.latitude) !== this.state.setCoordinates.lat && parseFloat(this.props.activeNeighborhood.longitude) !== this.state.setCoordinates.lng) {
       context.setState( { setCoordinates : {
         lat: parseFloat(context.props.activeNeighborhood.latitude),
         lng: parseFloat(context.props.activeNeighborhood.longitude)
-        }
-      });
-      context.componentDidMount();
+          }
+        }, () => {
+          context.componentDidMount();
+        });
+      }
     }
   }
 
@@ -46,7 +65,8 @@ export class GoogleMap extends Component {
 
 function mapStateToProps(state) {
   return {
-    activeNeighborhood: state.activeNeighborhood
+    activeNeighborhood: state.activeNeighborhood,
+    defaultCoordinate: state.defaultCoordinate
   };
 }
 
