439663f222da58efee2ed2e27d04b4e0cb57ef6a
Bruce <brucegraham11@gmail.com>
2017-03-03 18:39:37 -0800

    New Footer component.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <section id="bottom">
+        <div className="container wow fadeInDown">
+          <div className="row"></div>
+        </div>
+      </section>
+
+      <footer id="footer" className="midnight-blue">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">
+                <h4 className="white-text"> &copy; 2017 Neighborly. All Rights Reserved </h4>
+            </div>
+            <div className="col-sm-6">
+              <ul className="pull-right">
+                <li><a href="https://github.com/Stegothesis/Community" target="_blank">Check us out on GitHub<i className="fa fa-github"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+module.exports = Footer;
