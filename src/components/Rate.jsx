61417609a66b9dc4d956d387afe4d16d2c08563a
Bruce <brucegraham11@gmail.com>
2017-03-03 18:34:14 -0800

    Removal of React components that are no longer used.

--- src/components/Rate.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Rate = (props) => {
-  return <h1>Hello Rate</h1>;
-  //if onSubmit,
-  //this.props.dispatch(addReview(data))
-}
-
-module.exports = Rate;
--- src/components/User.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const User = (props) => {
-  return <h1>Hello, User</h1>;
-}
-
-module.exports = User;
