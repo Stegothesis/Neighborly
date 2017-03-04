dbaf77b53aa521dfa003fac67fd43df7f3269c7c
Michael Chiang <michiang@ucsd.edu>
2017-03-03 19:27:16 -0800

    Implement getZillowDemographics API call

--- server/apihelpers.js
+++ server/apihelpers.js
@@ -27,6 +27,27 @@ exports.getZillowHoods = function(city, state, callback) {
   });
 }
 
+exports.getZillowDemographics = function(neighborhood, city, callback) {
+   var options = {
+    url: 'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=' + zillowApiKey + '&neighborhood=' + neighborhood + '&city=' + city
+  }
+  request(options, function(err, res, body) {
+    if(err) {
+      callback(err, null);
+    } else {
+      parseString(body, function(err, obj) {
+        console.log('***ZILLOW DEMOGRAPHICS***', obj);
+        var response = obj['Demographics:demographics'].response;
+        if (response) {
+          callback(null, obj['Demographics:demographics'].response[0].pages[0].page[2].tables[0].table[0].data[0].attribute);
+        } else {
+          callback(null, "Call Lmit Exceeded");
+        }
+      });
+    }
+  });
+}
+
 //WalkScore requires an address in order to get back walkability scores for each neighborhood
 //Need to find reverse geocoding API in order to get the address from currently
 //pulled latitude and longitude from the Zillow API
