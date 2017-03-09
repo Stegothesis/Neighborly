724572a005367216315e9cf6b5b8a8b9b17859f9
Michael Chiang <michiang@ucsd.edu>
2017-03-08 17:34:15 -0800

    Implement autocomplete search feature to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,60 @@
-import React from 'react';
-import Auth from './Auth.jsx'
+import React, { Component } from 'react';
+import Auth from './Auth.jsx';
+import { hashHistory } from 'react-router';
+import { push } from 'react-router-redux';
 
-export const Navbar = () => {
+
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      city: '',
+      state:'',
+      location: '',
+      autocompleteNav: ''
+    };
+
+    this.onLocationInputChange = this.onLocationInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    let input = document.getElementById('navbar-input');
+    const options = {
+      types: ['(cities)'],
+      componentRestrictions: {country: "us"}
+    };
+    let autocompleteNav = new google.maps.places.Autocomplete(input, options);
+    this.setState({autocompleteNav: autocompleteNav});
+  }
+
+  onLocationInputChange(event) {
+    this.setState({location: event.target.value})
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    const context = this;
+    google.maps.event.addListener(this.state.autocompleteNav, 'place_changed', function() {
+      fillInAddress();
+    });
+    function fillInAddress() {
+      let place = context.state.autocompleteNav.getPlace();
+      console.log('PLACE', place);
+      let cityAuto = place.address_components[0].long_name;
+      let stateAuto = place.address_components[2].short_name;
+      context.setState({
+        city: cityAuto,
+        state: stateAuto,
+        location: ''
+      }, () => {
+        hashHistory.push(`/city/${context.state.city}/${context.state.state}`);
+      });
+    }
+  }
+
+  render() {
   return (
     <header id="header">
       <nav className="navbar navbar-inverse" role="banner">
@@ -16,6 +69,22 @@ export const Navbar = () => {
             <a className="navbar-brand" href="/"><img src="images/logo.png" className="image1" alt="logo"/></a>
           </div>
 
+          <div className="collapse navbar-collapse navbar-center">
+            <form onSubmit={(e)=>this.onFormSubmit(e)} className="input-group">
+              <input
+                placeholder="Enter a location"
+                className="form-control"
+                id="navbar-input"
+                type="text"
+                value={this.state.location}
+                onChange={this.onLocationInputChange}
+              />
+              <span className="input-group-btn">
+                <button id = "navbutton" type="submit" className="btn btn-primary">Submit</button>
+               </span>
+              </form>
+             </div>
+
           <div className="collapse navbar-collapse navbar-right">
             <ul className="nav navbar-nav">
               <li className="active"><a href="/">Search</a></li>
@@ -26,9 +95,9 @@ export const Navbar = () => {
       </nav>
     </header>
   )
+}
 };
 
-module.exports = Navbar;
 
 
 
